3b3fb91fb7519644bc8bfb837fb8492c55ecf916
gaganng <48212601+gaganng@users.noreply.github.com>
2020-05-25 23:03:24 +0530

    Update report_email.py

--- report_email.py
+++ report_email.py
@@ -1,10 +1,10 @@
-from datetime import date
-import os
 #By Gagan Gundala
-#Meant for Learning Purpose, not using for Coursera Submission.
-#This code is highly in-efficient and not written with correct programming practices
+#Meant for Learning Purpose.I do not recommend using it for Coursera Submission.
+#This code is in-efficient and not written with correct programming practices
 #But it works!!!!
 #!/usr/bin/env python3
+from datetime import date
+import os
 import reports
 import emails
 
